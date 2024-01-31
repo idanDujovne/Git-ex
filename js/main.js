3db22045455af2f0e3268ef1a5242f8207d2f727
Idan Dujovne <idan.duj@gmail.com>
2024-01-31 23:14:43 +0200

    Improve onClick function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 function onBallClick(elBall) {
     var ballSize = +elBall.innerText + 50
+    if (ballSize > 400) ballSize = 100
     elBall.style.height = ballSize + 'px'
     elBall.style.width = ballSize + 'px'
     elBall.innerText = ballSize
