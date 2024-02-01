2a2067828f340eda3460316efcf51c0cfe5f1937
Idan Dujovne <idan.duj@gmail.com>
2024-02-01 11:05:34 +0200

    Make third ball swap first two balls

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 'use strict'
 
 function onBallClick(elBall) {
-    var ballSize = +elBall.innerText + getRandomInt(20, 61)
+    const ballSize = +elBall.innerText + getRandomInt(20, 61)
     if (ballSize > 400) ballSize = 100
     elBall.style.height = ballSize + 'px'
     elBall.style.width = ballSize + 'px'
     elBall.innerText = ballSize
 
     elBall.style.backgroundColor = getRandomColor()
+}
+
+function swapColors() {
+    const elBalls = document.querySelectorAll('.ball')
+    const ball1Size = +elBalls[0].innerText
+    const ball1Color = elBalls[0].style.backgroundColor
+
+    const ball2Size = +elBalls[1].innerText
+    const ball2Color = elBalls[1].style.backgroundColor
+
+    modifyBall(elBalls[0], ball2Size, ball2Color)
+    modifyBall(elBalls[1], ball1Size, ball1Color)
+}
+
+function modifyBall(elBall, size, color) {
+    elBall.style.height = size + 'px'
+    elBall.style.width = size + 'px'
+    elBall.innerText = size
+    elBall.style.backgroundColor = color
 }
